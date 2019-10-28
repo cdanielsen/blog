25b4be64b49fd3f66eaae33a4d3a611e9fcb50f8
Christian Danielsen <christian.danielsen@insitu.com>
2019-10-28 12:33:20 -0700

    WIP first pass at generated resume

---
+++ src/pages/resume/index.js
@@ -0,0 +1,65 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import credentials from "./resumeContent"
+import Layout from "../../components/layout"
+
+const {
+  tagLine,
+  webdevExperience,
+  technicalSkills,
+  essentialSkills,
+  education,
+} = credentials
+
+export default function ResumePage({ location }) {
+  return (
+    <Layout location={location} includeHeader={false}>
+      <h1>Christian Danielsen</h1>
+      <p>{tagLine}</p>
+      <h3>Technical Skills</h3>
+      {technicalSkills.map(({ label, content }, idx) => (
+        <p key={idx}>
+          <strong>{label}</strong>
+          <br />
+          {content.join(", ")}
+        </p>
+      ))}
+      <h3>Essential Human / Developer Skills</h3>
+      <ul>
+        {essentialSkills.map((essentialSkill, idx) => (
+          <li key={idx}>{essentialSkill}</li>
+        ))}
+      </ul>
+      <h3>Web Development Experience</h3>
+      {webdevExperience.map((role, idx) => (
+        <div key={idx}>
+          <strong>{role.title}</strong>
+          <br />
+          <em>
+            {role.company} ({role.startDate} - {role.endDate})
+          </em>
+          <ul>
+            {role.highlights.map((highlight, idx) => {
+              return <li key={idx}>{highlight}</li>
+            })}
+          </ul>
+        </div>
+      ))}
+      <h3>Education</h3>
+      {education
+        .sort(({ date1 }, { date2 }) => date1 - date2)
+        .reverse()
+        .map(({ date, duration, institution, location, title }, idx) => (
+          <p key={idx}>
+            {date} - {title}
+            {duration ? ` (${duration})` : ""}, {institution}, {location}
+          </p>
+        ))}
+    </Layout>
+  )
+}
+
+ResumePage.propTypes = {
+  location: PropTypes.object.isRequired,
+}
---
+++ src/pages/resume/resumeContent.js
@@ -0,0 +1,174 @@
+// Resume Content
+
+export default {
+  tagLine: "Web Developer | JavaScript Specialist | Quality Mindset",
+  education: [
+    {
+      type: "degree",
+      title: "BA, International Relations",
+      institution: "UC Davis",
+      location: "Davis, CA",
+      date: 2005,
+    },
+    {
+      type: "degree",
+      title: "M.Ed, Curriculum and Instruction",
+      institution: "Portland State University",
+      location: "Portland, OR",
+      date: 2009,
+    },
+    {
+      type: "certificate",
+      title: "Full Stack Development Course",
+      duration: "700 hrs",
+      institution: "Epicodus",
+      location: "Portland, OR",
+      date: 2014,
+    },
+    {
+      type: "certificate",
+      title: "React/Redux Intensive Workshop",
+      duration: "40 hrs",
+      institution: "Alchemy Code Lab",
+      location: "Portland, OR",
+      date: 2017,
+    },
+    {
+      type: "certificate",
+      title: "GraphQL Intensive Workshop",
+      duration: "16 hrs",
+      institution: "Cascadia JS 2019",
+      location: "Seattle, WA",
+      date: 2019,
+    },
+  ],
+  essentialSkills: [
+    "Efficient researcher",
+    "Constant and humble learner",
+    "Skilled communicator across mediums, formats and audience types",
+    "Strong interpersonal skills",
+  ],
+  technicalSkills: [
+    {
+      label: "Languages and Frameworks",
+      content: [
+        "JavaScript/ES6",
+        "Node.js",
+        "Express.js",
+        "React/Redux",
+        "GraphQL",
+        "PostgreSQL",
+      ],
+    },
+    {
+      label: "Testing",
+      content: [
+        "Jest",
+        "Ava",
+        "Enzyme",
+        "Mocha",
+        "Sinon",
+        "Cypress.io",
+        "Postman",
+        "TestRail",
+      ],
+    },
+    {
+      label: "Tooling and DevOps",
+      content: ["Git/Gitflow", "Docker", "Jenkins", "TeamCity"],
+    },
+    {
+      label: "Workflow",
+      content: [
+        "Scrum/KanBan",
+        "Pair Programming",
+        "Atlassian Tools (Jira, Confluence, Bitbucket, Crucible)",
+        "GitHub",
+        "PivotalTracker",
+        "Trello",
+      ],
+    },
+  ],
+  webdevExperience: [
+    {
+      company: "Insitu",
+      title: "Software Developer III (Remote)",
+      startDate: "June 2018",
+      endDate: "Present",
+      highlights: [
+        "Led development of a microservice (node.js, express, jest) for processing large quantities of raw geospatial data and imagary for downstream consumption and visualization",
+        "Created code generation templates (yeoman) for several common project types (backend service, api test), dramatically reducing time required and bugs in creating new projects",
+        "Championing critical tooling improvement: establishing a binary repository manager for capturing common functionality into packages that can be shared across projects and teams",
+        "Led overhaul of comprehensive suite of 300+ UI automation tests for an enterprise level cloud storage platform, resulting in dramatic improvements in test reliability, performance, and maintainability",
+      ],
+    },
+    {
+      company: "InComm",
+      title: "Software Engineer in Test",
+      startDate: "May 2016",
+      endDate: "May 2018",
+      highlights: [
+        "Transitioned to leadership role in the extension, maintenance and integration of automated UI and API testing frameworks. Reduced weekly regression testing load from days to hours",
+        "Leading effort to standardize and implement modern JavaScript project best practices across a suite of front end development products",
+        "Contributed to development of a prototyped continuous delivery pipeline allowing for safe, automated deployments of a microservice to production on merging of a pull request",
+        "Provided training, code reviews and mentorship to new QA engineers",
+        "Developed automated feature branch validation system by integrating VC host (GitHub) and CI tool (TeamCity)",
+      ],
+    },
+    {
+      company: "InComm",
+      title: "Associate QA Engineer",
+      startDate: "December 2014",
+      endDate: "May 2016",
+      highlights: [
+        "Collaborated with senior SDET to build end-to-end automated UI testing framework for customer-facing and internal applications using Protractor, Jasmine, and npm libraries",
+        "Designed API testing framework using npm libraries (ava, request-promise, yeoman) and custom JavaScript to dramatically improve API layer test coverage. Integrated tests into TeamCity continuous integration server.",
+        "Daily experience with an agile sprint development cycle and associated processes/tools (git flow, project management, build chain/deployment, virtual machines and environments)",
+        "Conducted increasingly complex exploratory test sessions for bug and feature stories as a QA member of an 8-person cross functional product team",
+        "Contributed regular input and added value to agile ceremonies (sprint planning, retrospectives) and other specialized working groups",
+      ],
+    },
+    {
+      company: "Outdoor Project",
+      title: "Frontend Web Development Intern",
+      startDate: "October 2014",
+      endDate: "November 2014",
+      highlights: [
+        "Contributed to mobile-friendly refactor of CSS code using Sass/SMACCS",
+        "Studied fundamentals of Drupal 7 CMS",
+        "Developed style guide demonstrating standards for common modules",
+      ],
+    },
+  ],
+  communityContributions: [
+    {
+      type: "event",
+      title: "Hack the Dot",
+      role: "Lead Organizer",
+      location: "Portland, OR",
+      description:
+        "Planned and coordinated a beginner friendly one-night hackathon around building a website for a mystery domain name",
+    },
+    {
+      type: "talk",
+      title: "Front End PDX",
+      role: "speaker",
+      location: "Portland, OR",
+      description:
+        "Prepared and gave a detailed presentation on using tooling to create deterministic dependency trees for JavaScript projects",
+    },
+    {
+      type: "workshop",
+      title: "ACT-W Portland",
+      role: "instructor",
+      location: "Portland, OR",
+      description:
+        "Prepared and gave a detailed tutorial on testing web applications with cypress.io",
+    },
+  ],
+  personalInterests: [
+    "Long Distance Backpacking",
+    "Urban Dodgeball",
+    "High Risk Juggling",
+  ],
+}
